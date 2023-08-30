762b5a133a79cb4bfb7021d2bb0913a067334127
juhyung1991 <juhyung91@naver.com>
2023-08-30 10:22:33 +0900

    동적 계획법 DP

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -5,141 +5,38 @@
 #include <queue>
 using namespace std;
 #include <thread>
+#include <windows.h>
 
 // 그래프/트리 응용
-// 오늘의 주제 : 최소 스패닝 트리 (Minimum Spanning Tree)
+// 오늘의 주제 : 동적 계획범 (DP)
 
-class DisjointSet
+int cache[50][50];
+
+int combination(int n, int r)
 {
-public:
-	DisjointSet(int n) : _parent(n), _rank(n, 1)
-	{
-		for (int i = 0; i < n; i++)
-			_parent[i] = i;
-	}
-
-	// 조직 폭력배 구조?
-	// [1]		[3]
-	// [2]	 [4][5][0]
-	// 		    
-
-	// 니 대장이 누구니?
-	int Find(int u)
-	{
-		if (u == _parent[u])
-			return u;
-
-		//_parent[u] = Find(_parent[u]);
-		//return _parent[u];
-
-		return _parent[u] = Find(_parent[u]);
-	}
-
-	// u와 v를 합친다
-	void Merge(int u, int v)
-	{
-		u = Find(u);
-		v = Find(v);
-
-		if (u == v)
-			return;
-
-		if (_rank[u] > _rank[v])
-			swap(u, v);
-
-		// rank[u] <= rank[v] 보장됨
-		_parent[u] = v;
-
-		if (_rank[u] == _rank[v])
-			_rank[v]++;
-	}
-
-private:
-	vector<int> _parent;
-	vector<int> _rank;
-};
-
-
-struct Vertex
-{
-	// int data;
-
-};
-
-vector<Vertex> vertices;
-vector<vector<int>> adjacent; // 인접 행렬
-
-void CreateGraph()
-{
-	vertices.resize(6);
-	adjacent = vector<vector<int>>(6, vector<int>(6, -1));
-
-	adjacent[0][1] = adjacent[1][0] = 15;
-	adjacent[0][3] = adjacent[3][0] = 35;
-	adjacent[1][2] = adjacent[2][1] = 5;
-	adjacent[1][3] = adjacent[3][1] = 10;
-	adjacent[3][4] = adjacent[4][3] = 5;
-	adjacent[3][5] = adjacent[5][3] = 10;
-	adjacent[5][4] = adjacent[4][5] = 5;
-
-}
-
-struct CostEdge
-{
-	int cost;
-	int u;
-	int v;
-
-	bool operator<(CostEdge& other)
-	{
-		return cost < other.cost;
-	}
-
-};
-
-int Kruskal(vector<CostEdge>& selected)
-{
-	int ret = 0;
-
-	selected.clear();
-
-	vector<CostEdge> edges;
-
-	for (int u; u < adjacent.size(); u++)
-	{
-		for (int v; v < adjacent[u].size(); v++)
-		{
-			if(u > v)
-				continue;
-			int cost = adjacent[u][v];
-			if(cost == -1)
-				continue;
-				
-			edges.push_back(CostEdge{cost, u, v});
-		}
-	}
+	//기저 사례
+	if(r == 0 || n == r)
+		return 1;
 	
-	std::sort(edges.begin(), edges.end());
+	//이미 답을 구한 적 있으면 바로 반환
+	int& ret = cache[n][r];
+	
+	if(ret != -1)
+		return ret;
+	//새로 답을 구해서 캐시에 저장
 
-	DisjointSet sets(vertices.size());
-
-	for (CostEdge& edge : edges)
-	{
-		//같은 그룹이면 스킵(안 그러면 사이클 발생)
-		if(sets.Find(edge.u) == sets.Find(edge.v))
-			continue;
-
-		// 두 그룹을 합친다.
-		sets.Merge(edge.u, edge.v);
-		selected.push_back(edge);
-		ret += edge.cost;
-	}
-
-	return ret;
-
+	return ret = combination(n - 1, r - 1) + combination(n - 1, r);
 }
 
 int main()
 {
+	::memset(cache, -1, sizeof(cache));
+
+	__int64 start = GetTickCount64();
+	int lotto = combination(3, 2);
+	__int64 end = GetTickCount64();
+
+	cout << end - start << "ms" << endl;
+
 
 }
