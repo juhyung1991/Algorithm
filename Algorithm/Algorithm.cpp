4f7f377d280f54e4ba66b7752af95826a0c08be7
juhyung1991 <juhyung91@naver.com>
2023-10-25 09:52:02 +0900

    DP 개념 복습

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -7,10 +7,13 @@ using namespace std;
 #include <thread>
 #include <windows.h>
 
-// 그래프/트리 응용
-// 오늘의 주제 : 동적 계획범 (DP)
+//오늘의 주제 : 동적 계획법 (DP)
 
+//메모리제이션 (memoization)
 int cache[50][50];
+
+
+int g_count;
 
 int combination(int n, int r)
 {
@@ -20,10 +23,13 @@ int combination(int n, int r)
 	
 	//이미 답을 구한 적 있으면 바로 반환
 	int& ret = cache[n][r];
-	
 	if(ret != -1)
 		return ret;
+
 	//새로 답을 구해서 캐시에 저장
+
+	if(n == 2 && r == 1)
+		g_count++;
 
 	return ret = combination(n - 1, r - 1) + combination(n - 1, r);
 }
@@ -33,10 +39,12 @@ int main()
 	::memset(cache, -1, sizeof(cache));
 
 	__int64 start = GetTickCount64();
-	int lotto = combination(3, 2);
+
+	int lotto = combination(45, 6);
+
 	__int64 end = GetTickCount64();
 
-	cout << end - start << "ms" << endl;
-
+	cout <<end - start << " ms" << endl;
+	cout << g_count << " count" << endl;
 
 }
