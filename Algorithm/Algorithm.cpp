fa37af37366b0b98a3c8d6dcac84ccb40c4c6b8d
juhyung1991 <juhyung91@naver.com>
2023-10-25 11:02:45 +0900

    LIS

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -7,44 +7,69 @@ using namespace std;
 #include <thread>
 #include <windows.h>
 
-//오늘의 주제 : 동적 계획법 (DP)
+// 오늘의 주제 : 동적 계획법(DP)
+// LIS (Longest Increasing Sequence)
 
-//메모리제이션 (memoization)
-int cache[50][50];
+// Seq : 1 9 2 5 7
+// 부분 수열 : 일부(0개 이상) 숫자를 지우고 남은 수열
+// ex) 1 2 5
+// ex) 1 9 5 7
+// 순 증가 부분 수열
+// ex) 1 2 5
 
+// LIS 제일 긴 [순 증가 부분 수열]의 길이
+// 1 2 4 7 : 길이 4
 
-int g_count;
+int cache[100];
+vector<int> seq;
 
-int combination(int n, int r)
+int LIS(int pos)
 {
-	//기저 사례
-	if(r == 0 || n == r)
-		return 1;
-	
-	//이미 답을 구한 적 있으면 바로 반환
-	int& ret = cache[n][r];
+	// 기저 사항
+	//if(pos == seq.size() - 1)
+	//	return 1;
+
+	//캐시 확인
+	int& ret = cache[pos];
 	if(ret != -1)
 		return ret;
 
-	//새로 답을 구해서 캐시에 저장
+	//구하기
 
-	if(n == 2 && r == 1)
-		g_count++;
+	// Seq : 1 9 2 5 7
+	// 1 9 = 2
+	// 1 2 4 7 = 4
+	// 1 5 7 = 3
+	// 1 7 = 2
 
-	return ret = combination(n - 1, r - 1) + combination(n - 1, r);
+	// LIS(0) = LIS(1) + 1
+	// LIS(1) = 1
+
+	//최소 seq[pos]은 있으니 1부터 시작
+	ret = 1;
+
+	//구하기
+	for (int next = pos + 1; next < seq.size(); next++)
+	{
+		if (seq[pos] < seq[next])
+		{
+			ret = max(ret, 1 + LIS(next));
+		}
+	}
+
+	return ret;
 }
 
 int main()
 {
-	::memset(cache, -1, sizeof(cache));
+	::memset(cache, -1 , sizeof(cache));
 
-	__int64 start = GetTickCount64();
+	seq = vector<int>{10 ,1, 9, 2, 5, 7};
 
-	int lotto = combination(45, 6);
+	int ret = 0;
+	for(int pos = 0 ; pos < seq.size(); pos++)
+		ret = max(ret, LIS(pos));
+	
 
-	__int64 end = GetTickCount64();
-
-	cout <<end - start << " ms" << endl;
-	cout << g_count << " count" << endl;
-
+	cout << ret << " count " << endl;
 }
