97426c2d21be59c4a55f1f98b91dd00b988e3941
juhyung1991 <juhyung91@naver.com>
2023-10-26 10:44:13 +0900

    Tic-Tae-Toe

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -8,80 +8,146 @@ using namespace std;
 #include <windows.h>
 
 // 오늘의 주제 : 동적 계획법(DP)
-// TRIANGLE_PATH
-// - (0, 0)부터 시작해서 아래 or 아래우측으로 이동 가능
-// - 만나는 숫자를 모두 더함
-// - 더한 숫자가 최대가 되는 경로? 합? 
+// TIC-TAE-TOE
 
-// 6
-// 1 2
-// 3 7 4
-// 9 4 1 7
-// 2 7 5 9 4
+//[.][.][.]
+//[.][o][x]
+//[.][.][.]
 
-int N;
-vector<vector<int>> board;
-vector<vector<int>> cache;
-vector<vector<int>> nextX;
-int path(int y, int x)
+//[.][.][.][.][.][.][o][x][012] => 3^9 = 19683
+
+int HashKey(const vector<vector<char>>& board)
 {
-	//기저 사항
-	//if(y == N - 1)
-	//	return board[y][x];
-	if(y == N)
-		return 0;
+	int ret = 0;
+
+	for (int y = 0; y < 3; y++)
+	{
+		for (int x = 0; x < 3; x++)
+		{
+			ret = ret * 3;
+
+			if(board[y][x] == 'o')
+				ret += 1;
+			else if(board[y][x] == 'x')
+				ret += 2;
+		}
+	}
+	return ret;
+}
+
+
+vector<vector<char>> board;
+int cache[19683];
+enum 
+{
+	DEFAULT = 2,
+	WIN = 1,
+	DRAW = 0,
+	LOSE = -1
+};
+
+bool IsFinished(const vector<vector<char>>& board, char turn)
+{
+	//좌우
+	for(int i = 0 ; i < 3 ; i++)
+	{
+		if(board[i][0] == turn && board[i][1] == turn&& board[i][2] == turn)
+		return true;
+	}
+
+	//상하
+	for (int i = 0; i < 3; i++)
+	{
+		if (board[0][i] == turn && board[1][i] == turn && board[2][i] == turn)
+			return true;
+	}
+
+	//대각선
+	if (board[0][0] == turn && board[1][1] == turn && board[2][2] == turn)
+		return true;
+	if (board[0][2] == turn && board[1][1] == turn && board[2][0] == turn)
+		return true;
+
+	return false;
+}
+
+int CanWin(vector<vector<char>>& board, char turn)
+{
+	//기저 사례
+	if(IsFinished(board, 'o' + 'x' - turn))
+		return LOSE;
+
+
 	//캐시 확인
-	int& ret = cache[y][x];
-	if(ret != -1)
+	int key = HashKey(board);
+
+	int& ret = cache[key];
+	if(ret != DEFAULT)
 		return ret;
-
-	//적용
-	//board[y][x] + path(y + 1, x);
-	//board[y][x] + path(y + 1, x + 1);
-	//경로기록
 	
-		int nextBottom = path(y + 1, x);
-		int nextBottomRight = path(y + 1, x + 1);
+	//풀기
+	//[.][x][.]
+	//[.][o][.]
+	//[.][.][.]
+	int minValue = DEFAULT;
 
-		if(nextBottom > nextBottomRight)
-			nextX[y][x] = x;
-		else
-			nextX[y][x] = x + 1;
+	for (int y = 0; y < 3; y++)
+	{
+		for (int x = 0; x < 3; x++)
+		{
+			if(board[y][x] != '.')
+				continue;
 
+			//착수
+			board[y][x] = turn;
 
+			// 확인
+			minValue = min(minValue, CanWin(board, 'o' + 'x' - turn)); // 상대방이 패배하는게 제일 좋은 케이스
 
-	return 	ret = board[y][x] + max(nextBottom, nextBottomRight);
+			// 취소
+			board[y][x] = '.';
+		}
+	}
+
+	if(minValue == DRAW || minValue == DEFAULT)
+		return ret = DRAW;
+
+	return ret = - minValue;
 }
 
 int main()
 {
-	board = vector<vector<int>>
+	board = vector<vector<char>>
 	{
-		{6},
-		{1, 2},
-		{3, 7, 4},
-		{9, 4, 1, 7},
-		{2, 7, 5, 9, 4}
-
+		{'o', 'x', '.'},
+		{'.', 'o', '.'},
+		{'x', '.', 'x'}
 	};
 
-	N = board.size();
-	cache = vector<vector<int>>(N, vector<int>(N, -1));
-	nextX = vector<vector<int>>(N, vector<int>(N));
+	// 1byte
+	//memset(cache, -2, sizeof(cache)); 불가능
+	//memset(cache, 0, sizeof(cache)); // 가능 0 0 0 0 0000 1byte나 4byte나 다 0으로 채워져있어서 똑같음
+	//memset(cache, -1, sizeof(cache)); // 가능 0b11111111 -1은 전부 1로 채워져있는거여서 똑같기 때문에 가능
 
-	int ret = path(1, 0);
-
-	cout << ret << " count" << endl;
-
-	//경로 만들기
-	int y = 1;
-	int x = 0;
-
-	while (y < N)
+	for (int i = 0; i < 19683; i++)
 	{
-		cout << board[y][x] << " -> ";
+		cache[i] = DEFAULT;
+	}
 
-		x = nextX[y][x];
-		y++;
+	int win = CanWin(board, 'o');
+
+	switch (win)
+	{
+	case WIN:
+		cout <<"win" << endl;
+		break;
+	case DRAW:
+		cout << "Draw" << endl;
+		break;
+	case LOSE:
+		cout << "lose" << endl;
+		break;
+	default:
+		break;
 	}
 }
